5f7f4944cbed4c80533bedfd923113014fc3a18f
jxmiechx <78773815+jxmiechx@users.noreply.github.com>
2021-04-28 20:23:39 -0400

    updated formatting on the nav

--- header.php
+++ header.php
@@ -10,15 +10,17 @@
             <div id="banner">
         <h1 style="text-align:center"><span class="d-block p-2 bg-dark text-white">Project 03</h1>
         <nav>
-            <ul>
-                <li><a href="index.html">About The Project</a></li>
-                <li><a href="quiz.html">What's My Major?</a></li>
-                <li><a href="collegeofengineering.html">College of Engineering</a></li>
-                <li><a href="collegeofnursing.html">College of Nursing</a></li>
-                <li><a href="collegeofliberalartsandscience.html">College of Liberal Arts and Science</a></li>
-                <li><a href="schoolofbusiness.html">School Of Business</a></li>
-                <li><a href="php.html">Connect</a></li>
-            </ul>
+                  <ul class="nav justify-content-center">
+                      <p><br></p>
+                      <li class="nav-item"><a class="nav-link active" aria-current="page" href="index.html">About The Project</a></li>
+                      <li class="nav-item"><a class="nav-link active" aria-current="page" href="quiz.html">What's My Major?</a></li>
+                      <li class="nav-item"><a class="nav-link active" aria-current="page" href="collegeofengineering.html">College of Engineering</a></li>
+                      <li class="nav-item"><a class="nav-link active" aria-current="page" href="collegeofnursing.html">College of Nursing</a></li>
+                      <li class="nav-item"><a class="nav-link active" aria-current="page" href="collegeofliberalartsandscience.html">College of Liberal Arts and Science</a></li>
+                      <li class="nav-item"><a class="nav-link active" aria-current="page" href="schoolofbusiness.html">School Of Business</a></li>
+                      <li class="nav-item"><a class="nav-link active" aria-current="page" href="php.html">Connect</a></li>
+                      <li class="nav-item"><a class="nav-link active" aria-current="page" href="quotes.html">Inspirational Quotes</a></li>
+               </ul>
         </nav>
         </div>
         
